8870bc3d4ea20cde2743b65a853d895aab20e89c
hnewey7 <hnewey7@gmail.com>
2024-03-22 13:00:40 +0000

    Adding file to store server connection details.

---
+++ SERVER_DETAILS.py
@@ -0,0 +1,29 @@
+'''
+Storing details of MySQL server.
+
+Created on Friday 22nd March 2024.
+@author: Harry New
+
+'''
+
+# SSH details.
+standard_server_details = {
+  "account":"",
+  "password":""
+}
+
+# MySQL details.
+mysql_server_details = {
+  "account":"",
+  "password":""
+}
+
+# - - - - - - - - - - - - - - - -
+
+def get_standard_server_details() -> dict:
+  """ Getting standard SSH details."""
+  return standard_server_details
+
+def get_mysql_server_details() -> dict:
+  """ Getting MySQL server details."""
+  return mysql_server_details
