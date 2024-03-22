35aaebbe15caf0b413e7014f8709c8965a5e65b1
hnewey7 <hnewey7@gmail.com>
2024-03-22 12:59:21 +0000

    Adding author to the SQL query.

--- sql_queries/CREATE_STRATEGIES_TABLE.sql
+++ sql_queries/CREATE_STRATEGIES_TABLE.sql
@@ -1,6 +1,8 @@
+-- Creating Strategies table
 CREATE TABLE Strategies (
 Id INT AUTO_INCREMENT,
 Name VARCHAR(20) NOT NULL,
 Description TEXT NOT NULL,
+Author VARCHAR(20) NOT NULL,
 PRIMARY KEY (Id)
 );
