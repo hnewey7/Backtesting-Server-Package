e9c20671958f33a7d1873b4bf156aed54967287b
hnewey7 <hnewey7@gmail.com>
2024-04-09 22:45:49 +0100

    test: creating directory for unit tests

---
+++ tests/BacktestingServer_test.py
@@ -0,0 +1,25 @@
+'''
+Module for testing the BacktestingServer object.
+
+Created on Tuesday 9th April 2024.
+@author: Harry New
+
+'''
+
+import pytest
+from backtesting_server import BacktestingServer
+from SERVER_DETAILS import get_standard_server_details, get_mysql_server_details
+
+# - - - - - - - - - - - - - - - - - - -
+
+def test_BacktestingServer_init() -> None:
+  """ Testing the initialisation of the BacktestingServer object."""
+  # Creating backtesting server object.
+  server = BacktestingServer(standard_details=get_standard_server_details(),sql_details=get_mysql_server_details())
+  
+  # Handling assertions.
+  assert server.standard_details
+  assert server.sql_details
+  for key in ["server","username","password"]:
+    assert server.standard_details[key] 
+    assert server.sql_details[key]
