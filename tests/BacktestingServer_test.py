1ce2d930a19645a60f0860732eb9126422afa505
hnewey7 <hnewey7@gmail.com>
2024-04-09 23:26:28 +0100

    test: adding unit tests for connect method

--- tests/BacktestingServer_test.py
+++ tests/BacktestingServer_test.py
@@ -16,10 +16,32 @@ def test_BacktestingServer_init() -> None:
   """ Testing the initialisation of the BacktestingServer object."""
   # Creating backtesting server object.
   server = BacktestingServer(standard_details=get_standard_server_details(),sql_details=get_mysql_server_details())
-  
+
   # Handling assertions.
   assert server.standard_details
   assert server.sql_details
   for key in ["server","username","password"]:
     assert server.standard_details[key] 
     assert server.sql_details[key]
+
+def test_BacktestingServer_connect() -> None:
+  """ Testing the connect method within the BacktestingServer object."""
+  # Creating backtesting server object.
+  server = BacktestingServer(standard_details=get_standard_server_details(),sql_details=get_mysql_server_details())
+  # Connecting to the server.
+  channel, cursor = server.connect()
+
+  # Handling assertions.
+  assert channel
+  assert cursor
+
+def test_BacktestingServer_connect_invalid() -> None:
+  """ Testing invalid details with the BacktestingServer object."""
+  # Creating backtesting server object.
+  server = BacktestingServer(standard_details={"server":"", "username":"", "password":""},sql_details={"server":"", "username":"", "password":""})
+  # Connecting to the server.
+  channel, cursor = server.connect()
+
+  # Handling assertions.
+  assert channel == None
+  assert cursor == None
