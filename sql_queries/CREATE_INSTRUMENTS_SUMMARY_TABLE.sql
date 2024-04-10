a4e8497ff471a7f6b4bc20fb2542609a0b009e1b
hnewey7 <hnewey7@gmail.com>
2024-04-10 21:49:09 +0100

    chore: adding not null to values within table

--- sql_queries/CREATE_INSTRUMENTS_SUMMARY_TABLE.sql
+++ sql_queries/CREATE_INSTRUMENTS_SUMMARY_TABLE.sql
@@ -1,7 +1,7 @@
 -- Creating Instrument Summary table.
 CREATE TABLE InstrumentSummary (
 Id INT AUTO_INCREMENT,
-Name VARCHAR(20),
-Epic VARCHAR(100),
+Name VARCHAR(20) NOT NULL,
+Epic VARCHAR(100) NOT NULL,
 PRIMARY KEY (Id)
 );
