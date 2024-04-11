69869ea83d39db91a55f3ab1ff57a7545742c1e0
hnewey7 <hnewey7@gmail.com>
2024-04-11 23:39:10 +0100

    chore: initial uploading historical data methods
    Initial functionality for uploading historical data to the server. Basic
    methods for checking if summary table exists, creating the summary table
    and checking if the instrument is in summary.

--- src/backtesting_server/main.py
+++ src/backtesting_server/main.py
@@ -13,6 +13,8 @@ import sys
 import paramiko.channel
 import pymysql
 import pymysql.cursors
+import ig_package
+import pandas as pd
 
 # - - - - - - - - - - - - - -
 
@@ -72,6 +74,72 @@ class BacktestingServer():
     except:
       logger.info("Unable to connect to MySQL server.")
       return None, None
+
+  def upload_historical_data(self, instrument:ig_package.Instrument, dataset:pd.DataFrame) -> None:
+    """ Uploading historical data to the backtesting server.
+    
+        Parameters
+        ----------
+        instrument: ig_package.Instrument
+          Instrument the historical data corresponds to.
+        dataset: pd.DataFrame
+          DataFrame containing the data to be uploaded."""
+    # Checking if historical data summary exists.
+    if not self._check_historical_data_summary_exists():
+      # Creating summary table.
+      self._create_historical_data_summary()
+    # Checking if data is already present.
+    self._check_instrument_in_historical_data(instrument)
+
+  def _check_historical_data_summary_exists(self) -> bool:
+    """ Checking if the historical data summary table exists on the MySQL server.
+        
+        Returns
+        -------
+        bool
+          Depending on whether the summary table exists or not."""
+    try:
+      self.cursor.execute('SELECT * FROM HistoricalDataSummary;')
+      logger.info("Historical Data Summary exists.")
+      return True
+    except:
+      logger.info("Historical Data Summary does not exist.")
+      return False
+
+  def _check_instrument_in_historical_data(self, instrument:ig_package.Instrument) -> bool:
+    """ Checking if instrument is already in historical data.
+        
+        Parameters
+        ----------
+        instrument: ig_package.Instrument
+          Instrument to be checked.
+        
+        Returns
+        -------
+        bool
+          Boolean depending if instrument is present in historical data."""
+    # Checking historical data summary for instrument.
+    self.cursor.execute(f'SELECT * FROM HistoricalDataSummary WHERE Epic="{instrument.epic}";')
+    result = cursor.fetchall()
+    if len(result) == 0:
+      logger.info(f"Instrument ({instrument.name}) could not be found in the historical data summary.")
+      return False
+    else:
+      logger.info(f"Instrument ({instrument.name}) is already in the historical data summary.")
+      return True
+
+  def _create_historical_data_summary(self) -> None:
+    """ Creating the historical data summary on the MySQL server."""
+    try:
+      self.cursor.execute('CREATE TABLE HistoricalDataSummary (\
+      ID INT NOT NULL AUTO_INCREMENT,\
+      InstrumentName VARCHAR(20),\
+      Epic VARCHAR(100),\
+      PRIMARY KEY (ID)\
+      );')
+      logger.info("Created Historical Data Summary.")
+    except:
+      logger.info("Failed to create Historical Data Summary.")
 
 # - - - - - - - - - - - - - -
     
