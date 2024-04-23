f664c75cd4bf63ecb042aa7fddc6d768bb06325c
hnewey7 <hnewey7@gmail.com>
2024-04-23 16:16:35 +0100

    docs: updating for new release

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,7 @@ release = '0.1.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-  "myst-nb",
+  "myst_nb",
   "autoapi.extension",
   "sphinx.ext.napoleon",
   "sphinx.ext.viewcode"
