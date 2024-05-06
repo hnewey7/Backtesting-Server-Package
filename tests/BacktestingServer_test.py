2a12ae7f088be29f93bb3928b2627eb599787e06
hnewey7 <hnewey7@gmail.com>
2024-05-06 22:53:45 +0100

    test: adding get historical data unit test

--- tests/BacktestingServer_test.py
+++ tests/BacktestingServer_test.py
@@ -623,3 +623,26 @@ def test_upload_live_data() -> None:
 
 # - - - - - - - - - - - - - - - - - - -
 # GET HISTORICAL DATA TESTS.
+
+def test_get_historical_data() -> None:
+  """ Testing get historical data from Backtesting Server."""
+  # Creating backtesting server object.
+  server = BacktestingServer(standard_details=get_standard_server_details(),sql_details=get_mysql_server_details())
+  # Connecting to the server.
+  server.connect(database="test")
+  # Resetting tables.
+  reset_mysql_tables(server)
+
+  # Getting test instrument.
+  ig_details = get_ig_details()
+  ig = ig_package.IG(API_key=ig_details['key'],username=ig_details['username'],password=ig_details['password'],acc_type=ig_details["acc_type"],acc_number=ig_details["acc_number"])
+  test_instrument = ig.search_instrument('Bitcoin')
+
+  # Uploading live data.
+  server.upload_live_data(ig,[test_instrument],capture_period=30)
+
+  # Getting historical data.
+  dataframe = server.get_historical_data(test_instrument)
+
+  # Handling checks.
+  assert len(dataframe) > 0
